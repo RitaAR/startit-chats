81d68c4acda31778f79c9f4ee2af35e64f58c341
RitaAR <54497047+RitaAR@users.noreply.github.com>
2020-01-10 14:31:47 +0200

    ielasīt, sūtīt

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
-from flask import Flask, render_template
-
+from flask import Flask, json, jsonify, render_template, request
 
 app = Flask('app')
 
 
 @app.route('/')
-def index_page():
-  return render_template("index.html")
+def index_lapa():
+  return render_template('index.html')
 
+
+@app.route('/health')
+def health_check():
+  return "OK"
+
+
+@app.route('/chats/lasi')
+def ielasit_chatu():
+  chata_rindas = []
+  with open("chats.txt", "r", encoding="UTF-8") as f:
+    for rinda in f:
+      chata_rindas.append(rinda)
+  return jsonify({"chats": chata_rindas})
+
+
+@app.route('/chats/suuti', methods=['POST'])
+def suutiit_zinju():
+
+  dati = request.json
+  
+  with open("chats.txt", "a", newline="") as f:
+    f.write(dati["chats"] + "\n")
+
+  return ielasit_chatu()
+  
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
