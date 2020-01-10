00d7812d7bc52bd04edf35fdd2305f772bb95a86
RitaAR <54497047+RitaAR@users.noreply.github.com>
2020-01-10 16:04:43 +0200

    cats strādā

--- static/chats.js
+++ static/chats.js
@@ -1,4 +1,5 @@
-const ATJAUNOT = 50000;
+const ATJAUNOT = 5000;
+
 
 /*
 Ielādē tērzēšanas datus no servera
@@ -12,14 +13,49 @@ async function lasiChatu() {
     await lasiChatu();
 }
 
-function raadiChatuVienkarsi(dati) {
-    const jaunaRinda = "<br/>";
-    let chats = "";
-    let chataDiv = document.getElementById("chats");
 
-    for (let rinda of dati["chats"]) {
+function raadiChatuVienkarsi(dati) {
+    var jaunaRinda = "<br/>";
+    var chats = "";
+    var chataDiv = document.getElementById("chats");
+    for (var rinda of dati["chats"]) {
         chats = chats + rinda + jaunaRinda;
     }
     chataDiv.innerHTML = chats;
 }
 
+
+/*
+Publicē tērzēšanas ziņas datus uz serveri
+*/
+async function suutiZinju() {
+    // Nolasa ievades lauka saturu
+    let zinjasElements = document.getElementById("zinja");
+    let zinja = zinjasElements.value;
+    // izdzēš ievades lauku
+    zinjasElements.value = "";
+
+    const atbilde = await fetch('/chats/suuti', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "chats": zinja })
+    });
+    const datuObjekts = await atbilde.json();
+
+    // parāda jauno chata saturu
+    raadiChatuVienkarsi(datuObjekts);
+}
+
+
+// Ērtības funkcionalitāte
+// Atrod ievades lauku
+var ievadesLauks = document.getElementById("zinja");
+// Gaida signālu no klaviatūras, ka ir nospiests Enter taustiņš
+ievadesLauks.addEventListener("keyup", function(event) {
+  // Numur 13 ir "Enter" taustiņš
+  if (event.keyCode === 13) {
+    suutiZinju();
+  }
+})
