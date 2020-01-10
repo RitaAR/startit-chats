dd2de0b3d9a2a317ac062cb9626d859764723cb1
RitaAR <54497047+RitaAR@users.noreply.github.com>
2020-01-10 17:01:58 +0200

    skaisti!

--- static/chats.js
+++ static/chats.js
@@ -8,7 +8,7 @@ Uzstāda laiku pēc kāda atkārtoti izsaukt šo pašu funkciju
 async function lasiChatu() {
     const atbilde = await fetch('/chats/lasi');
     const datuObjekts = await atbilde.json();
-    raadiChatuVienkarsi(datuObjekts);
+    raadiChataRindas(datuObjekts);
     await new Promise(resolve => setTimeout(resolve, ATJAUNOT));
     await lasiChatu();
 }
@@ -45,7 +45,7 @@ async function suutiZinju() {
     const datuObjekts = await atbilde.json();
 
     // parāda jauno chata saturu
-    raadiChatuVienkarsi(datuObjekts);
+    raadiChataRindas(datuObjekts);
 }
 
 
@@ -59,3 +59,30 @@ ievadesLauks.addEventListener("keyup", function(event) {
     suutiZinju();
   }
 })
+
+function raadiChataRindas(dati) {
+    const chatUL = document.getElementById("chats");
+    // novaacam ieprieksheejo saturu
+    while (chatUL.firstChild) {
+        chatUL.firstChild.remove();
+    }
+    for (let rinda of dati["chats"]) {
+      chatLI = izveidoJaunuRindu(rinda);
+      chatUL.appendChild(chatLI);
+    }
+    // noskrolleejam uz leju pie peedeejaa chata texta
+    var chatScrollBox = chatUL.parentNode;
+    chatScrollBox.scrollTop = chatScrollBox.scrollHeight;
+  }
+  
+  
+  function izveidoJaunuRindu(zinja) { 
+    let newLI = document.createElement("li");
+    newLI.className = "left clearfix"
+    let newDiv = document.createElement("div"); 
+    newDiv.className = "chat-body clearfix"
+    let newContent = document.createTextNode(zinja); 
+    newLI.appendChild(newDiv); 
+    newDiv.appendChild(newContent); 
+    return newLI;
+  }
